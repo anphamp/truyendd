51869b09da622f966e3a9b3635cc7b49ab8aa129
anphamp <anem48933@gmail.com>
2024-01-18 17:51:27 +0700

    hhhh

---
+++ truyen.php/truyen.php
@@ -0,0 +1,24 @@
+<?php
+ echo '[
+    {
+        "tenTruyen":"Ta Một Bước Làm Quỷ Vương",
+        "tenChap":"Chapter 85",
+        "linkAnh":"https://st.nhattruyenmax.com/data/comics/106/ta-mot-buoc-lam-quy-vuong.jpg"
+        },
+  {
+        "tenTruyen":"Ender Geister",
+        "tenChap":"Chapter 81",
+        "linkAnh":"https://st.nhattruyento.com/data/comics/38/ender-geister.jpg"
+        },
+  {
+        "tenTruyen":"Trùm Phản Diện Muốn Tìm Chết!",
+        "tenChap":"Chapter 35",
+        "linkAnh":"https://st.nhattruyento.com/data/comics/55/trum-phan-dien-muon-tim-chet-9696.jpg"
+        }
+ ]
+ ';
+
+
+
+
+?>
